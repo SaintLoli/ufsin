46cb41801ee357aeff173e5371ae58a2fb757921
SaintLoli <88616759+SaintLoli@users.noreply.github.com>
2025-02-05 12:21:34 +0300

    + видеокарта

--- classes/PCInfo.py
+++ classes/PCInfo.py
@@ -9,17 +9,24 @@ class PCInfo:
         self.video_card = ''
         self.other = []
 
-        self.register = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+        self.registry = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
 
 
         """
         вызов функций на получение информации из регистра
         """
         self.get_processor_info()
+        self.get_video_card_info()
 
     def get_processor_info(self):
-        key = OpenKey(self.register, r"HARDWARE\DESCRIPTION\System\CentralProcessor\0")
+        key = OpenKey(self.registry, r"HARDWARE\DESCRIPTION\System\CentralProcessor\0")
         self.processor = QueryValueEx(key, "ProcessorNameString")[0]
+
+    def get_video_card_info(self):
+        path_to_video_card = QueryValueEx(OpenKey(self.registry, r"HARDWARE\DEVICEMAP\VIDEO"), "\Device\Video0")[0]
+        path_to_video_card = path_to_video_card.replace("\Registry\Machine\System", "")
+        path_to_video_card = r"SYSTEM" + path_to_video_card
+        self.video_card = QueryValueEx(OpenKey(self.registry, path_to_video_card), "HardwareInformation.AdapterString")[0]
 
     def get_pc_processor(self):
         return self.processor
