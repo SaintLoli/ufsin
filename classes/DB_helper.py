4abb4bfed9329b93d8581589389247f84e3c6837
SaintLoli <88616759+SaintLoli@users.noreply.github.com>
2025-02-05 13:32:59 +0300

    + dbhelper шаблон

--- classes/DB_helper.py
+++ classes/DB_helper.py
@@ -2,41 +2,5 @@ import sqlite3
 
 class DBHelper:
     def __init__(self):
-        self.con = sqlite3.connect('data/ItemBase.db')
+        self.con = sqlite3.connect('DB/UFSIN_DB.db')
         self.cur = self.con.cursor()
-
-
-    def delete_table(self, table):
-        self.cur.execute(f"DELETE FROM {table}")
-        self.con.commit()
-
-    def add_info_to_main_table(self, data_dict):
-        data_dict["title"] = data_dict["title"].replace("'", "`")
-        print(data_dict['title'])
-
-        self.cur.execute(f"""
-                            INSERT INTO Main_Table(group_id, type, title) VALUES
-                            (
-                                '{data_dict["group_id"]}', '{data_dict["type"]}', '{data_dict["title"]}'
-                            )
-                        """)
-        self.con.commit()
-
-
-    def add_info_to_data_table(self, data_dict):
-        self.delete_table('Data_Table')
-        for skin in data_dict:
-            print(f"""
-                                       INSERT INTO Data_Table(skin_title, min_float, stickers, skin_price) VALUES
-                                       (
-                                           '{skin["title"]}', '{skin["min_float"]}', '{skin["stickers"]}', '{skin["skin_price"]}'
-                                       )
-                                   """)
-
-            self.cur.execute(f"""
-                                       INSERT INTO Data_Table(skin_title, min_float, stickers, skin_price) VALUES
-                                       (
-                                           '{skin["title"].replace("'", "`")}', '{skin["min_float"]}', '{str(skin["stickers"]).replace("'", '"')}', '{skin["skin_price"]}'
-                                       )
-                                   """)
-            self.con.commit()
