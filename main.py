5163bb60ece6cd0bd4f8bc6ada59fc148d256308
SaintLoli <88616759+SaintLoli@users.noreply.github.com>
2025-02-06 12:43:15 +0300

    reg rabotaet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__, template_folder="layout")
@@ -8,13 +8,17 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/registration")
+@app.route("/registration", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        FIO = request.form["FIO"]
+        username = request.form['login']
+        password = request.form['pass']
+
+        print(FIO, username, password)
     return render_template("registration.html")
 
 
-def rtest():
-    return "1231231232132123123213"
 
 
 if __name__ == '__main__':
