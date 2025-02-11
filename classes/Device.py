3d75b2e4c241c3837e034f46d0f26450dedf4595
SaintLoli <88616759+SaintLoli@users.noreply.github.com>
2025-02-11 12:56:46 +0300

    zayebis` vse zdelal

--- classes/Device.py
+++ classes/Device.py
@@ -1,5 +1,9 @@
 class Device:
-    def __init__(self, name, type, status):
+    def __init__(self, name, type, status, year='', s_number=''):
         self.name = name
         self.type = type
         self.status = status
+
+        self.year = year
+        self.s_number = s_number
+        self.id = ''
--- classes/routes.py
+++ classes/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from classes.DB_helper import  DBHelper
 from classes.PCInfo import PCInfo
 from classes.Device import Device
+from classes.TABLENAMES import TABLENAMES
 
 app = Flask(__name__, template_folder="../layout")
 database = DBHelper()
@@ -47,35 +48,38 @@ def register():
 @app.route("/admin_home")
 def admin_panel():
     PC = database.get_computer_info(int(USER_ID))
-    print(PC)
+    fill_devices()
 
+    # return render_template("admin_home.html",
+    #                        motherboard=PC[1],
+    #                        cpu=PC[0],
+    #                        gpu=PC[2],
+    #                        ram=PC[3],
+    #                        s_number=PC[5],
+    #                        year=PC[4],
+    #                        monitor=database.get_item("monitor", int(USER_ID)),
+    #                        tel=database.get_item("tel", int(USER_ID)))
     return render_template("admin_home.html",
-                           motherboard=PC[1],
-                           cpu=PC[0],
-                           gpu=PC[2],
-                           ram=PC[3],
-                           s_number=PC[5],
-                           year=PC[4],
-                           monitor=database.get_item("monitor", int(USER_ID)),
-                           tel=database.get_item("tel", int(USER_ID)))
+                           devices=DEVICES)
 
 
 @app.route("/admin_home/staff")
 def staff():
-
     return render_template("staff.html")
+
 
 @app.route("/admin_home/sklad")
 def sklad():
-
     return render_template("sklad.html")
+
+
 @app.route("/admin_home/otchet")
 def otchet():
-
     return render_template("otchet.html")
+
+
 @app.route("/admin_home/departments")
 def departments():
-
     return render_template("departments.html")
 
 
@@ -99,8 +103,20 @@ def check_data():
                                          gpu=PC.video_card)
 
 
-def fill_devices(user_id):
-    ...
+def fill_devices():
+    global DEVICES
+    DEVICES.clear()
+    id = 1
+
+    PC = database.get_computer_info(int(USER_ID))
+    DEVICES.append(Device(", <br>".join(PC[:4]), "Системный блок", "В эксплуатации", PC[4], PC[5]))
+    DEVICES[-1].id = id
+    id += 1
+
+    for table_name in TABLENAMES.keys():
+        DEVICES.append(Device(database.get_item(table_name, int(USER_ID)), TABLENAMES[table_name], "В эксплуатации"))
+        DEVICES[-1].id = id
+        id += 1
 
 
 
